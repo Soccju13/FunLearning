10b680e1531997793772a9259c3ab98ece09c3e8
Socccju13 <juan2jo@gmail.com>
2019-06-21 00:58:05 -0500

    10%

--- app/src/main/java/aess/unt/funlearning/MainActivity.java
+++ app/src/main/java/aess/unt/funlearning/MainActivity.java
@@ -4,24 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     Button cambio;
+    TextView lbregistrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//Acá veremos el cambio al menu del usuario
         cambio = findViewById(R.id.btn_registrar);
-
         cambio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent cambio = new Intent(MainActivity.this,menu_usuario.class);
                 startActivity(cambio);
+            }
+        });
+//Acá iremos al menu registrar
+        lbregistrar = findViewById(R.id.txt_registrar);
+        lbregistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent lbregistrar1 = new Intent(MainActivity.this,activity_register.class);
+                startActivity(lbregistrar1);
             }
         });
     }
